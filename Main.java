bbdbd26e7be417a1dfe25870047812c8a4c57436
ghiblismi <99272230+ghiblismi@users.noreply.github.com>
2022-03-23 16:09:42 -0400

    Note

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String [] args) {
+		
+		System.out.println("Name: ");
+		name = keyIn.readLine();
+		
+		System.out.println("Email Address: ");
+		email = keyIn.readLine();
+		
+		System.out.println("Phone Number: ");
+		phone = keyIn.readLine();
+		
+		
+	
+	}
+}
