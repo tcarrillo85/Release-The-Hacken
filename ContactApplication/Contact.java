824bb5c81f5a5d32bbcfc095cbddc494b68acecc
tcarrillo85 <101580553+tcarrillo85@users.noreply.github.com>
2022-03-23 11:54:56 -0700

    Add files via upload

---
+++ ContactApplication/Contact.java
@@ -0,0 +1,21 @@
+/*
+ * Parent Class
+ */
+public class Contact {
+	
+	protected final int contactId;
+	static int nextid = 1;
+	private String = name;
+	private String = number;
+	private LocalDateTime createdAt = LocalDateTime.Now();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
